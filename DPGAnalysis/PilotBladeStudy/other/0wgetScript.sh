9325d4abd69f31cdcad7d9386438ea530c23d11f
Tamas Vami <vami.tamas@wigner.mta.hu>
2016-10-12 16:23:30 +0200

    Update 0wgetScript.sh

--- DPGAnalysis/PilotBladeStudy/other/0wgetScript.sh
+++ DPGAnalysis/PilotBladeStudy/other/0wgetScript.sh
@@ -2,3 +2,4 @@ wget https://raw.githubusercontent.com/tvami/cmssw80X-PilotBlade/PilotBladeStudy
 wget https://raw.githubusercontent.com/tvami/cmssw80X-PilotBlade/PilotBladeStudy/DPGAnalysis/PilotBladeStudy/other/Makefile.arch
 wget https://raw.githubusercontent.com/tvami/cmssw80X-PilotBlade/PilotBladeStudy/DPGAnalysis/PilotBladeStudy/other/PilotHistoMaker.C
 wget https://raw.githubusercontent.com/tvami/cmssw80X-PilotBlade/PilotBladeStudy/DPGAnalysis/PilotBladeStudy/other/PilotHistoMaker.h
+wget https://raw.githubusercontent.com/tvami/cmssw80X-PilotBlade/PilotBladeStudy/DPGAnalysis/PilotBladeStudy/other/0plottingScript.sh

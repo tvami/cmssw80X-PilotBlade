1892b8ec9da474b396c641125258020a8b5154f9
Tamas Vami <vami.tamas@wigner.mta.hu>
2016-10-26 13:36:47 +0200

    Update PilotLSHistoMaker.C

--- DPGAnalysis/PilotBladeStudy/other/LSHistoMaker/PilotLSHistoMaker.C
+++ DPGAnalysis/PilotBladeStudy/other/LSHistoMaker/PilotLSHistoMaker.C
@@ -45,7 +45,7 @@ void digis(bool saveAll, std::string save_dir, const char* format, TChain* filec
     Long64_t reduceFraction=0;
     Int_t skipFiles=0;
     
-    int _detids[] = { 344132868, 344134148, 344131076, 344132100, 344130820, 344131844, 1 };
+    int _detids[] = { 344132868, 344134148, 344131076, 344132100, 344130820, 1, 344131844 };
     //int _detids[] = { 344130820, 344131844, 344132868, 344133892, 344131076, 344132100, 344133124, 344134148, 1 };
     std::vector<int> detids(_detids, _detids + sizeof(_detids) / sizeof(int) );
     
@@ -213,7 +213,7 @@ void clusters(bool saveAll, std::string save_dir, const char* format, TChain* fi
     Long64_t reduceFraction=0;
     Int_t skipFiles=0;
     
-    int _detids[] = { 344132868, 344134148, 344131076, 344132100, 344130820, 344131844, 1 };
+    int _detids[] = { 344132868, 344134148, 344131076, 344132100, 344130820, 1, 344131844 };
     //int _detids[] = { 344130820, 344131844, 344132868, 344133892, 344131076, 344132100, 344133124, 344134148, 1 };
     std::vector<int> detids(_detids, _detids + sizeof(_detids) / sizeof(int) );
     
